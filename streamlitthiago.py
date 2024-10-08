5a234b24b5ef487025abc1c93216f506acc9b01e
Thiago Pereira <166023602+thiagopintopereira@users.noreply.github.com>
2024-10-08 15:15:30 -0300

    Update streamlitthiago.py

--- streamlitthiago.py
+++ streamlitthiago.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 st.title('DASHBOARD DO RECLAME AQUI')
 
-empresa = st.selectbox('SELECIONE A EMPRESA', ['', 'Hapvida', 'Ibyte', 'Nagem'])
+empresa = st.sidebar('SELECIONE A EMPRESA', ['', 'Hapvida', 'Ibyte', 'Nagem'])
 
 # Carregar os arquivos CSV
 file_hapvida = pd.read_csv('HAPVIDA_ETL.csv')
