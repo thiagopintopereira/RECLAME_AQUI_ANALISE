3b3c2566571f71df5fdad0d207d7d35ad747f065
Thiago Pereira <166023602+thiagopintopereira@users.noreply.github.com>
2024-10-08 10:50:42 -0300

    Update streamlitthiago.py

--- streamlitthiago.py
+++ streamlitthiago.py
@@ -4,43 +4,33 @@ import numpy as np
 import scipy
 import streamlit as st
 
-st.title('DASHBOARD ANÁLISE RECLAME AQUI')
+st.title('DASHBOARD DE RECLAMAÇÕES NO RECLAME AQUI')
 
-# Carregar os arquivos CSV
-file_hapvida = r'C:\Users\thiag\OneDrive\Área de Trabalho\Pyhton\HAPVIDA_ETL.csv'
-file_nagem = r'C:\Users\thiag\OneDrive\Área de Trabalho\Pyhton\NAGEM_ETL.csv'
-file_ibyte = r'C:\Users\thiag\OneDrive\Área de Trabalho\Pyhton\IBYTE_ETL.csv'
 
-# Ler os DataFrames
-df_hapvida = pd.read_csv(file_hapvida)
-df_nagem = pd.read_csv(file_nagem)
-df_ibyte = pd.read_csv(file_ibyte)
+empresa = st.selectbox('SELECIONE A EMPRESA',
+                        ['Hapvida','Ibyte', 'Nagem'])
 
-# Seleção da empresa
-empresa = st.selectbox('SELECIONE A EMPRESA', ['Hapvida', 'Ibyte', 'Nagem'])
+# Carregar o arquivo CSV
+file_hapvida = pd.read_csv(r'C:\Users\thiag\OneDrive\Área de Trabalho\Pyhton\HAPVIDA_ETL.csv')
+file_nagem = pd.read_csv(r'C:\Users\thiag\OneDrive\Área de Trabalho\Pyhton\NAGEM_ETL.csv')
+file_ibyte = pd.read_csv(r'C:\Users\thiag\OneDrive\Área de Trabalho\Pyhton\IBYTE_ETL.csv')
 
-# Seleção do status
-status = st.selectbox('SELECIONE O STATUS', ['Não respondida', 'Respondida', 'Resolvido', 'Em réplica', 'Não resolvido'], index=0)
+status = st.selectbox('SELECIONE O STATUS',
+                    ['Não respondida', 'Respondida',
+                    'Resolvido', 'Em réplica','Não resolvido'],
+                    index = 0)
 
-# Calcular total de reclamações
-total = (
-    df_hapvida['STATUS'].value_counts().sum() +
-    df_ibyte['STATUS'].value_counts().sum() +
-    df_nagem['STATUS'].value_counts().sum()
-)
-
-# Exibir total de reclamações
-col1, col2 = st.columns(2)
+col1 , col2 = st.columns(2)
 with col1:
-    st.metric(label='TOTAL RECLAMAÇÕES', value=total)
-
-# Calcular e exibir o total para o status selecionado
+    total = file_hapvida['STATUS'].value_counts().sum() + file_ibyte['STATUS'].value_counts().sum() + file_nagem.value_counts().sum()
+    st.metric(label='TOTAL RECLAMAÇÕES',
+            value=total)
 with col2:
-    temp_hapvida = df_hapvida['STATUS'].value_counts().get(status, 0)
-    temp_nagem = df_nagem['STATUS'].value_counts().get(status, 0)
-    temp_ibyte = df_ibyte['STATUS'].value_counts().get(status, 0)
-    
-    temp = temp_hapvida + temp_nagem + temp_ibyte
-    st.metric(label=status.upper(), value=temp)
+    temp = file_hapvida['STATUS'].value_counts().loc[status] + file_ibyte['STATUS'].value_counts().loc[status] + file_nagem['STATUS'].value_counts().loc[status]
+    st.metric(label= status.upper(),
+              value=temp)
 
 st.markdown('---')
+
+#### Series temporais Número de Reclamações
+
